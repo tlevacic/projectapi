cdf5c4b26fa7b2996929052ee752cbea14367350
Tin August Levačić <tilevaci@vsite.hr>
2020-04-09 16:45:02 -0700

    Added create account, generation password, and sending emails

--- routes/project.js
+++ routes/project.js
@@ -44,34 +44,32 @@ router.post('/create-project', async (req, res) => {
 
 router.get('/project', async (req, res, next) => {
     try {
-        const id = req.body.id;
-        if (id) {
+        const id = req.query.id;
+        
             const project = await Project.findOne({ _id: id });
             try {
                 res.json(project)
             } catch (error) {
                 res.json({ error: error })
             }
-        } else {
-            res.json({ status: 204, message: "Id not provided" })
         }
-    } catch (error) {
+    catch (error) {
         res.json({ error: error })
     }
 })
 
 router.post('/update-project', async (req, res, next) => {
     try {
-        const id = req.body.id;
+        const id = req.body.data.id;
         if (id) {
             const oldProject = await Project.findById(id);
 
 
-            const title = req.body.title ? req.body.title : oldProject.title;
-            const typeOfApp = req.body.typeOfApp ? req.body.typeOfApp : oldProject.typeOfApp;
-            const info = req.body.info ? req.body.info : oldProject.info;
-            const tech = req.body.tech ? req.body.tech : oldProject.tech;
-            const status = req.body.status ? req.body.status : oldProject.status;
+            const title = req.body.data.title ? req.body.data.title : oldProject.title;
+            const typeOfApp = req.body.data.typeOfApp ? req.body.data.typeOfApp : oldProject.typeOfApp;
+            const description = req.body.data.description ? req.body.data.description : oldProject.description;
+            const tech = req.body.data.tech ? req.body.data.tech : oldProject.tech;
+            const status = req.body.data.status ? req.body.data.status : oldProject.status;
 
             Project.findOneAndUpdate({ _id: id },
                 {
@@ -79,7 +77,7 @@ router.post('/update-project', async (req, res, next) => {
                     {
                         title: title,
                         typeOfApp: typeOfApp,
-                        info: info,
+                        description: description,
                         tech: tech,
                         status: status
                     }
--- routes/user.js
+++ routes/user.js
@@ -5,32 +5,63 @@ const Project = require('../models/Project');
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+var nodemailer = require('nodemailer');
 
 //Only admin can create new users--works!!
 router.post('/create-account', (req, res, next) => {
-    User.find({ email: req.body.email })
+    User.find({ email: req.body.data.email })
         .exec()
         .then(user => {
             if (user.length >= 1) {
-                return res.json({status: 422, message:"Email exists"})
+                return res.status(422).json({ status: 422, message: "Email exists" })
             } else {
-                bcrypt.hash(req.body.password, 10, async (err, hash) => {
+                let password = Math.random().toString(36).substring(8);
+
+                bcrypt.hash(password, 10, async (err, hash) => {
                     if (err) {
                         return res.status(500).json({
                             error: err
                         });
                     } else {
                         const user = new User({
-                            email: req.body.email,
+                            email: req.body.data.email,
                             password: hash,
-                            role: req.body.role,
-                            phone:req.body.phone,
-                            name: req.body.name
+                            role: req.body.data.role,
+                            phone: req.body.data.phone,
+                            name: req.body.data.name
                         });
                         user.save()
                             .then((result) => {
-                                res.json({ status:200,message: 'User created!'});
+                                let transporter = nodemailer.createTransport({
+                                    host: 'smtp.gmail.com',
+                                    port: 587,
+                                    secure: false,
+                                    requireTLS: true,
+                                    auth: {
+                                        user: 'projectschoolmanagement@gmail.com',
+                                        pass: 'qwe123QWE123'
+                                    }
+                                });
+                                
+                                let mailOptions = {
+                                    from: 'projectschoolmanagement@gmail.com',
+                                    to: `${user.email}`,
+                                    subject: 'Credentials',
+                                    text: `Hello, ${user.name},
+                                    You have been added to Project Management System,
+                                    please first time when you log in change your password on account page.
+                                    Password: ${password}
+                                    Feel free to contact us for any questions.`
+                                };
+                                
+                                transporter.sendMail(mailOptions, (error, info) => {
+                                    if (error) {
+                                        return console.log(error.message);
+                                    }
+                                    console.log('success');
+                                    return res.status(200).json({message: "User Created"})
+                                });
+                               
                             })
                             .catch((error) => {
                                 res.status(500).json({ error });
@@ -41,14 +72,44 @@ router.post('/create-account', (req, res, next) => {
         })
 
 })
+router.post('/reset-password', async (req, res, next) => {
+    const loginUser = await User.findOne({ _id: req.body.data.id })
+    try {
+        bcrypt.hash(req.body.data.password, 10, async (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err
+                });
+            } else {
+                User.findOneAndUpdate({ _id: req.body.data.id },
+                    {
+                        $set:
+                        {
+                            password: hash
+                        }
+                    },
+                    { new: true },
+                    (err, doc) => {
+                        if (err) {
+                            res.json({ status: 404, message: "Reset failed" })
+                        }
 
+                        res.status(200).json({ message: "Pasword reseted" })
+                    });
+            }
+        })
+    }
+    catch (error) {
+        return res.status(404).json({ status: 404, message: "Failed" })
+    }
+})
 //Login--works!!
 router.post('/login', async (req, res, next) => {
     const loginUser = await User.findOne({ email: req.body.email })
     try {
         bcrypt.compare(req.body.password, loginUser.password, (err, result) => {
             if (err) {
-                return res.json({status: 404, message:"Auth Failed"})
+                return res.json({ status: 404, message: "Auth Failed" })
             }
             if (result) {
                 const token = jwt.sign({
@@ -65,12 +126,12 @@ router.post('/login', async (req, res, next) => {
                     id: loginUser._id
                 });
             } else {
-                return res.json({status: 404, message:"Auth Failed"})
+                return res.status(404).json({ status: 404, message: "Auth Failed" })
             }
         })
     }
     catch (error) {
-         return res.json({status: 404, message:"Auth Failed"})
+        return res.status(404).json({ status: 404, message: "Auth Failed" })
     }
 })
 
@@ -78,7 +139,7 @@ router.post('/login', async (req, res, next) => {
 //Returns all users and projects--works!!!
 router.get('/all-users', async (req, res, next) => {
     try {
-        const users = await User.find().populate("projects").exec((err,result)=>{
+        const users = await User.find().populate("projects").exec((err, result) => {
             res.json(result)
         });
     } catch (error) {
@@ -88,43 +149,68 @@ router.get('/all-users', async (req, res, next) => {
 
 //Returns user information for populating form!!!
 router.get('/user-current', async (req, res, next) => {
-    if(req.body.id){
-        try {
-            const user = await User.findOne({_id: req.body.id});
-            res.json({
-                name: user.name,
-                email: user.email,
-                phone: user.phone
-            })
-        } catch (error) {
-            res.json(error)
-        }
-    }else{
+    try {
+        const user = await User.findOne({ _id: req.query.id });
         res.json({
-            status: 204,
-            message: "Id not provided" 
+            name: user.name,
+            email: user.email,
+            phone: user.phone
         })
+    } catch (error) {
+        res.json(error)
     }
+
 })
 
+
+//Update user info
+router.post('/user-update', async (req, res, next) => {
+    try {
+        User.findOneAndUpdate({ _id: req.body.data.id },
+            {
+                $set:
+                {
+                    name: req.body.data.name,
+                    phone: req.body.data.phone,
+                }
+            },
+            { new: true },
+            (err, doc) => {
+                if (err) {
+                    res.json({ status: 404, message: "Update not completed" })
+                }
+
+                res.send({
+                    name: doc.name,
+                    email: doc.email,
+                    phone: doc.phone
+                })
+            });
+    } catch (error) {
+        res.json(error)
+    }
+
+})
 
 
 //Returns one user with all projects--works!!
 //Input: userId
 router.get('/user', async (req, res, next) => {
     try {
-        const id=req.body.id;
-        if(id){
-            await User.findOne({_id: id}).populate("projects").exec(function(err,project) {
+        const id = req.body.id;
+        if (id) {
+            await User.findOne({ _id: id }).populate("projects").exec(function (err, project) {
                 res.json(project)
-              })
-        }else{
-            res.json({status: 204, message: "Id not provided"})
+            })
+        } else {
+            res.json({ status: 204, message: "Id not provided" })
         }
     } catch (error) {
-        res.json({error:error})
+        res.json({ error: error })
     }
 })
+
+
 
 
 
@@ -135,12 +221,12 @@ router.get('/user', async (req, res, next) => {
 /////////////////////////////
 /////////////////////////////
 /////////////////////////////
-router.post('/delete-users', async (req,res,next) =>{
+router.post('/delete-users', async (req, res, next) => {
     try {
         await User.deleteMany();
         res.json("Users deleted")
     } catch (error) {
-        res.json({error:error})
+        res.json({ error: error })
     }
 })
 
